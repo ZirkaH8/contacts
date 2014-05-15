824f039b1bb61c6a9cf5b6e694cfa5fed98f1cff
zirkah8 <zirkah8@gmail.com>
2014-05-16 00:01:11 +0300

    first commit

---
+++ js/confirm.js
@@ -0,0 +1,76 @@
+(function ($) {
+    Drupal.behaviors.addressbook = {
+        attach: function (context, settings) {
+
+            $("#delete").click(function(e){
+                if(!confirm('Aru you sure?')){e.preventDefault();}
+            });
+
+
+            function hide(){
+                $('.form-item-last-name').hide();
+                $('.form-item-first-name').hide();
+                $('#edit-field-category').hide();
+                $('#edit-field-photo').hide();
+                $('#field-phone-add-more-wrapper').hide();
+                $('#edit-field-email').hide();
+                $('#field-birth-day-add-more-wrapper').hide();
+                $('#edit-field-note').hide();
+                $('#edit-back').hide();
+                $('#edit-submit').hide();
+                $('#edit-next').hide();
+                $('input[name="field_photo_und_0_upload_button"]').hide();
+            }
+            if(typeof  page == 'undefined')
+            {
+                var page = 1;
+                pageCheck();
+                $('#edit-next').bind('click',function(e){
+                    page++;
+
+                    pageCheck();
+                    e.preventDefault();
+                });
+                $('#edit-back').bind('click',function(e){
+                    page--;
+
+                    pageCheck();
+                    e.preventDefault();
+                });
+            }
+
+            function pageCheck()
+            {
+                if (page == 1){
+
+                    hide();
+                    $('.form-item-first-name').show();
+                    $('.form-item-last-name').show();
+                    $('#edit-field-category').show();
+                    $('#edit-next').show();
+                }else if(page == 2)
+                {
+                    hide();
+                    $('#edit-field-photo').show();
+                    $('#edit-back').show();
+                    $('#edit-next').show();
+
+                }
+                else
+                {
+                    hide();
+                    $('#field-phone-add-more-wrapper').show();
+                    $('#edit-field-email').show();
+                    $('#field-birth-day-add-more-wrapper').show();
+                    $('#edit-field-note').show();
+                    $('#edit-back').show();
+                    $('#edit-submit').show();
+                }
+            }
+
+        }
+
+
+    };
+
+})(jQuery);
